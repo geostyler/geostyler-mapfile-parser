0f0392271ad3911614712add8eac46283a95c76e
Marc <marc.fasel@camptocamp.com>
2020-05-05 17:48:11 +0200

    Added function to convert RGB ranges

--- src/Useful.ts
+++ src/Useful.ts
@@ -9,3 +9,17 @@ export function rgbToHex(s: string): string {
   // eslint-disable-next-line no-bitwise
   return '#' + ((1 << 24) + (rgb[0] << 16) + (rgb[1] << 8) + rgb[2]).toString(16).toUpperCase().slice(1);
 }
+
+/**
+   * Convert a color range in RGB (R G B R G B) format to hexadecimal (#RRGGBB) array
+   *
+   * @param {string} s The strings representing the range of colors in RGB in the Mapfile file
+   * @return {string[]} The same colors as an array of strings in hexadecimal format
+   */
+  export function rgbRangeToHexArray(s: string): string[] {
+    const rgbrgb = s.split(' ').map(Number);
+    const lowerBoundColor = '#' + ((1 << 24) + (rgbrgb[0] << 16) + (rgbrgb[1] << 8) + rgbrgb[2]).toString(16).toUpperCase().slice(1);
+    const upperBoundColor = '#' + ((1 << 24) + (rgbrgb[3] << 16) + (rgbrgb[4] << 8) + rgbrgb[5]).toString(16).toUpperCase().slice(1);
+    return [lowerBoundColor, upperBoundColor]
+  }
+  
