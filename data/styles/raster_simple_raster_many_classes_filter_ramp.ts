b11a63657cfa6eed467ebe279db9421f8aa1857f
Marc <marc.fasel@camptocamp.com>
2020-05-05 16:23:49 +0200

    Added test for raster classes

---
+++ data/styles/raster_simple_raster_many_classes_filter_ramp.ts
@@ -0,0 +1,27 @@
+import { Style } from 'geostyler-style';
+
+const rasterStyle: Style = {
+  name: 'Simple raster',
+  rules: [{
+    name: 'Test raster',
+    symbolizers: [{
+      kind: 'Raster',
+      colorMap: {
+        type: 'ramp',
+        colorMapEntries: [{
+          color: '#000000',
+          quantity: 0
+        }, {
+          color: '#003200',
+          quantity: 100,
+          opacity: 1
+        }, {
+          color: '#003250',
+          quantity: 200
+        }]
+      },
+    }]
+  }]
+};
+
+export default rasterStyle;
