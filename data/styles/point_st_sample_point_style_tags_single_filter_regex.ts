ac7274ad44cf50fac29f9fae34f51c9fd22ea3b2
Benjamin Gerber <benjamin.gerber@camptocamp.com>
2020-05-15 11:09:49 +0200

    Debug mapfiles

--- data/styles/point_st_sample_point_style_tags_single_filter_regex.ts
+++ data/styles/point_st_sample_point_style_tags_single_filter_regex.ts
@@ -4,14 +4,14 @@ const pointStyle: Style = {
   name: 'point_st_sample_point_style_tags_single_filter_regex',
   rules: [{
     filter: [
-      '*=', ['FN_strMatches', 'OBJECTVAL', '/^(A|B)Anyword$/'], true
+      '*=', ['FN_strMatches', 'name', '/bus/'], true
     ],
     name: 'Test point',
     symbolizers: [{
       kind: 'Mark',
       wellKnownName: 'Circle',
       color: '#000000',
-      radius: 2.5,
+      radius: 4.25,
       rotate: 360,
       opacity: 1
     }],
