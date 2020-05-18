8d8ac1ce3201c9fefe8aa0982a1560309cfd13bd
Marc <marc.fasel@camptocamp.com>
2020-05-18 17:16:25 +0200

    Added unit tests for rasters

---
+++ data/styles/raster_simple_raster_many_classes_filter_values.ts
@@ -0,0 +1,25 @@
+import { Style } from 'geostyler-style';
+
+const rasterStyle: Style = {
+  name: 'raster_simple_raster_many_classes_filter_values',
+  rules: [{
+    name: 'Test raster',
+    symbolizers: [{
+      kind: 'Raster',
+      colorMap: {
+        type: 'values',
+        colorMapEntries: [{
+          color: '#00FF00',
+          quantity: 0,
+          opacity: 1
+        }, {
+          color: '#0000FF',
+          quantity: 1,
+          opacity: 1
+        }]
+      },
+    }]
+  }]
+};
+
+export default rasterStyle;
--- src/Useful.ts
+++ src/Useful.ts
@@ -4,8 +4,21 @@
    * @param {string} s The string representing the color in RGB in the Mapfile file
    * @return {string} The same color in hexadecimal format
    */
-export function rgbToHex(s: string): string {
-  const rgb = s.split(' ').map(Number);
-  // eslint-disable-next-line no-bitwise
-  return '#' + ((1 << 24) + (rgb[0] << 16) + (rgb[1] << 8) + rgb[2]).toString(16).toUpperCase().slice(1);
-}
+  export function rgbToHex(s: string): string {
+    const rgb = s.split(' ').map(Number);
+    // eslint-disable-next-line no-bitwise
+    return '#' + ((1 << 24) + (rgb[0] << 16) + (rgb[1] << 8) + rgb[2]).toString(16).toUpperCase().slice(1);
+  }
+
+/**
+   * Convert a color range in RGB (R G B R G B) format to hexadecimal (#RRGGBB) array
+   *
+   * @param {string} s The strings representing the range of colors in RGB in the Mapfile file
+   * @return {string[]} The same colors as an array of strings in hexadecimal format
+   */
+  export function rgbRangeToHexArray(s: string): string[] {
+    const rgbRange = s.split(' ').map(Number);
+    const lowerBoundColor = '#' + ((1 << 24) + (rgbRange[0] << 16) + (rgbRange[1] << 8) + rgbRange[2]).toString(16).toUpperCase().slice(1);
+    const upperBoundColor = '#' + ((1 << 24) + (rgbRange[3] << 16) + (rgbRange[4] << 8) + rgbRange[5]).toString(16).toUpperCase().slice(1);
+    return [lowerBoundColor, upperBoundColor]
+  }
